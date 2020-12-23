9c3a0b20e2aed5a150946d96569db02ddef9f10f
saichandrasekar <saichandrasekar@gmail.com>
2020-12-23 08:44:37 +0530

    Basic course exercise

---
+++ basics/simple.py
@@ -0,0 +1,57 @@
+print("Saichandrasekar", "Tm")
+
+print("Saichandrasekar", "Tm", sep="_", end="**")
+
+print("Meera my love")
+
+separator = "_"
+
+endValue = "$$"
+
+print("Saichandrasekar","TM","Meera my love", sep=separator, end=endValue)
+print("Thank you")
+
+print("Hello again",separator)
+
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n")
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2)
+
+print(type(4.))
+
+print(0x3AF)
+
+print(type(3.45e45))
+
+print(0b10)
+
+print('I am a string inside single quotes')
+
+print("I am a string inside double quotes")
+
+print('I am string inside "single quotes"')
+
+print("I am a string inside 'double quotes'")
+
+
+print(type(3.4))
+
+print(2 * 3 % 5)
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+
+print((25%13))
+
+__ = "sai"
+
+print(__)
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 4), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
